abc62e3de9f5e326fe00f9fb12e13d37580b9ce6
Nicolas Herbaut <nicolas.herbaut@gmail.com>
2022-10-21 09:46:07 +0200

    example tictactoe

--- src/test/java/fr/pantheonsorbonne/miage/TicTacToeBoardTest.java
+++ src/test/java/fr/pantheonsorbonne/miage/TicTacToeBoardTest.java
@@ -2,50 +2,51 @@ package fr.pantheonsorbonne.miage;
 
 import fr.pantheonsorbonne.miage.game.tictactoe.TicTacToe;
 import fr.pantheonsorbonne.miage.game.tictactoe.AbstractTicTacToe;
+import fr.pantheonsorbonne.miage.game.tictactoe.TicTacToeImpl;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ChifoumiBoardTest {
+class TicTacToeBoardTest {
 
     @Test
     public void testWinner1() {
-        TicTacToe board = new AbstractTicTacToe("XXXOXOXXO");
+        TicTacToe board = new TicTacToeImpl("XXXOXOXXO");
         assertEquals("XXX\nOXO\nXXO", board.toString());
         assertEquals('X', board.getWinner());
     }
 
     @Test
     public void testWinner2() {
-        TicTacToe board = new AbstractTicTacToe("XXOOOOXOX");
+        TicTacToe board = new TicTacToeImpl("XXOOOOXOX");
         assertEquals("XXO\nOOO\nXOX", board.toString());
         assertEquals('O', board.getWinner());
     }
 
     @Test
     public void testWinner3() {
-        TicTacToe board = new AbstractTicTacToe("XOOOXXOXX");
+        TicTacToe board = new TicTacToeImpl("XOOOXXOXX");
         assertEquals("XOO\nOXX\nOXX", board.toString());
         assertEquals('X', board.getWinner());
     }
 
     @Test
     public void testWinner4() {
-        TicTacToe board = new AbstractTicTacToe("XOOXOXOXX");
+        TicTacToe board = new TicTacToeImpl("XOOXOXOXX");
 
         assertEquals('O', board.getWinner());
     }
 
     @Test
     public void testNextPlayer() {
-        TicTacToe board = new AbstractTicTacToe("X...");
+        TicTacToe board = new TicTacToeImpl("X...");
 
         assertEquals('O', board.nextPlayer());
     }
 
     @Test
     public void testNextPlayer2() {
-        TicTacToe board = new AbstractTicTacToe("XXO.");
+        TicTacToe board = new TicTacToeImpl("XXO.");
 
         assertEquals('O', board.nextPlayer());
     }
