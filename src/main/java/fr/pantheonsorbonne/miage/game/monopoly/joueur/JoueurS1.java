07e5a3953c29ec1b579422c83123d01460ea617c
teodoraistrate <teodoraistrate63@yahoo.com>
2023-12-02 12:08:06 +0100

    methodes strategies

--- src/main/java/fr/pantheonsorbonne/miage/game/monopoly/joueur/JoueurS1.java
+++ src/main/java/fr/pantheonsorbonne/miage/game/monopoly/joueur/JoueurS1.java
@@ -1,6 +1,7 @@
 package fr.pantheonsorbonne.miage.game.monopoly.joueur;
 
 import java.util.List;
+import java.util.Map;
 
 import fr.pantheonsorbonne.miage.game.monopoly.plateau.Prison;
 import fr.pantheonsorbonne.miage.game.monopoly.plateau.cartes.CartePayerOuChance;
@@ -37,23 +38,34 @@ public class JoueurS1 extends Joueur {
         return false;
     }
 
-    
     @Override
-    public int choixNombreMaisonsAVendre() {
+    public List<Propriete> choixProprietesAHypothequer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'choixProprietesAHypothequer'");
+    }
+
+    @Override
+    public Map<Propriete, Integer> choixNombreMaisonsAVendre() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'choixNombreMaisonsAVendre'");
     }
 
     @Override
-    public int choixNombreHotelsAVendre() {
+    public List<Propriete> choixHotelsAVendre() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'choixNombreHotelsAVendre'");
+        throw new UnsupportedOperationException("Unimplemented method 'choixHotelsAVendre'");
     }
 
     @Override
-    public List<Propriete> choixProprietesAHypothequer() {
+    public boolean payerOuAttendre() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'choixProprietesAHypothequer'");
+        throw new UnsupportedOperationException("Unimplemented method 'payerOuAttendre'");
+    }
+
+    @Override
+    public boolean transformerProprieteEnPrison() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'transformerProprieteEnPrison'");
     }
 
     
