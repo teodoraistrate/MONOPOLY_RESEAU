a246cf663d4481e927e5a10e519715ab5cd6b1b9
Sara Kjallouch <skjallouch@yahoo.com>
2023-12-10 18:57:28 +0100

    MonopolyHostLocal

--- src/main/java/fr/pantheonsorbonne/miage/game/monopoly/jeu/MonopolyHostLocal.java
+++ src/main/java/fr/pantheonsorbonne/miage/game/monopoly/jeu/MonopolyHostLocal.java
@@ -1,4 +1,4 @@
-package fr.pantheonsorbonne.miage.game.monopoly.jeu;
+/*package fr.pantheonsorbonne.miage.game.monopoly.jeu;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -70,5 +70,94 @@ public class MonopolyHostLocal extends JeuMonopoly {
     }
 
     
+
+}
+*/
+
+package fr.pantheonsorbonne.miage.game.monopoly.jeu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.pantheonsorbonne.miage.game.monopoly.joueur.Joueur;
+import fr.pantheonsorbonne.miage.game.monopoly.joueur.JoueurS1;
+import fr.pantheonsorbonne.miage.game.monopoly.joueur.JoueurS2;
+import fr.pantheonsorbonne.miage.game.monopoly.joueur.JoueurS3;
+import fr.pantheonsorbonne.miage.game.monopoly.joueur.PasAssezArgentException;
+import fr.pantheonsorbonne.miage.game.monopoly.plateau.proprietes.Propriete;
+import fr.pantheonsorbonne.miage.game.monopoly.plateau.proprietes.Terrain;
+
+public class MonopolyHostLocal extends JeuMonopoly {
+
+    private static MonopolyHostLocal instance;
+    List<Joueur> listeJoueurs = new ArrayList<>();
+
+    public MonopolyHostLocal() {
+        JoueurS1 joueur1 = new JoueurS1("Joueur 1");
+        JoueurS2 joueur2 = new JoueurS2("Joueur 2");
+        JoueurS3 joueur3 = new JoueurS3("Joueur 3");
+
+        listeJoueurs.add(joueur1);
+        listeJoueurs.add(joueur2);
+        listeJoueurs.add(joueur3);
+    }
+
+    public static MonopolyHostLocal getInstance() {
+        if (instance == null) {
+            instance = new MonopolyHostLocal();
+        }
+        return instance;
+    }
+
+    public Joueur getJoueurParId(String idJoueur) {
+        for (Joueur j : listeJoueurs) {
+            if (j.getName().equals(idJoueur)) return j;
+        }
+        return null;
+    }
+
+    @Override
+    public List<Joueur> getListeJoueurs() {
+        return listeJoueurs;
+    }
+  
+    @Override
+    protected boolean askGetOutOfJail(String idJoueur) {
+        Joueur joueur = getJoueurParId(idJoueur);
+        return joueur.choixSortirPrison();
+    }
+
+    @Override
+    protected boolean askBuyProperty(String idJoueur, Propriete propriete) {
+        Joueur joueur = getJoueurParId(idJoueur);
+        return joueur.choixAcheterPropriete(propriete);
+    }
+
+    @Override
+    protected boolean askRemoveInstantlySquat(String idJoueur, Terrain proprieteSquatee) {
+        Joueur joueur = getJoueurParId(idJoueur);
+        return joueur.choixPayerOuAttendre();
+    }
+
+    @Override
+    protected void thinkAndDo(String idJoueur) {
+        Joueur joueur = getJoueurParId(idJoueur);
+
+        // Logique pour la réflexion et l'action du joueur
+        List<Propriete> proprietesAchete = joueur.choixProprietesARacheter();
+        for (Propriete p : proprietesAchete) {
+            boolean aAchete = askBuyProperty(idJoueur, p);
+            if (aAchete) {
+                try {
+                    joueur.acheterPropriete(p);
+                } catch (PasAssezArgentException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
 
 }
